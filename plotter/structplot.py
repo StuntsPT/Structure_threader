212c658a0fd5b98f82ab71b60d5fb4ea796811e1
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-05-05 14:37:09 +0100

    Improved plotting.

--- plotter/structplot.py
+++ plotter/structplot.py
@@ -20,35 +20,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def dataminer(indfile_name):
-    """Parses the output files of structure and faststructure and returns a 
+    """Parse the output files of structure and faststructure and return a
     numpy array with the individual clustering values and a list with the
     population names and number of individuals."""
+    # TODO: Add option to parse structure output nd turn in into faststructure, or even better parse it right away. Preferebly with automatic detection.
     indfile = indfile_name  # Placeholder code to insert function that identifies structure and faststructure outputs and uses the correct parsing function.
-    
+
     Qvalues = np.genfromtxt(indfile)
-    
+
     return Qvalues
 
+
 def plotter(Qvalues):
-    """Plots the Qvalues histogram"""
-    
+    """Plot the Qvalues histogram."""
+
     colors = ('#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c',
               '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928')
-    
+
     numinds = Qvalues.shape[0]
     for i in range(Qvalues.shape[1]):
         if i == 0:
-            plt.bar(range(numinds), Qvalues[:, i], facecolor=colors[i], edgecolor="none", width=1)
+            plt.bar(range(numinds), Qvalues[:, i], facecolor=colors[i],
+                    edgecolor="none", width=1)
             formerQ = Qvalues[:, i]
         else:
-            plt.bar(range(numinds), Qvalues[:, i], bottom=formerQ, facecolor=colors[i], edgecolor="none", width=1)
+            plt.bar(range(numinds), Qvalues[:, i], bottom=formerQ,
+                    facecolor=colors[i], edgecolor="none", width=1)
             formerQ = formerQ + Qvalues[:, i]
-    
+
+    plt.ylim(0, 1)
+
+    plt.yticks([])
+
+    # TODO: Add pop labels and split bars
     plt.show()
-    
+
 if __name__ == "__main__":
     data = dataminer("/home/francisco/Dropbox/Science/PhD/GBS/Analyses/clust6/structure/tests/k3.3.meanQ")
     plotter(data)
-    
-    
