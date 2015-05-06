b8f1e5decdc8798a7812e43c99da0552b3fac077
Diogo Silva <o.diogosilva@gmail.com>
2015-05-06 13:44:12 +0100

    dataminer now parses structure output as well. Added popfile optional argument to parse population information. Made some tweaks to the generated plot: (i) ggplot style; (ii) figure width updates with number of samples; (iii) Adds population boundary lines. #11

--- plotter/structplot.py
+++ plotter/structplot.py
@@ -19,44 +19,119 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import Counter
 
 
-def dataminer(indfile_name):
+def dataminer(indfile_name, fmt, popfile=None):
     """Parse the output files of structure and faststructure and return a
     numpy array with the individual clustering values and a list with the
-    population names and number of individuals."""
-    # TODO: Add option to parse structure output nd turn in into faststructure, or even better parse it right away. Preferebly with automatic detection.
-    indfile = indfile_name  # Placeholder code to insert function that identifies structure and faststructure outputs and uses the correct parsing function.
+    population names and number of individuals.
 
-    Qvalues = np.genfromtxt(indfile)
+    :param indfile_name: path to structure/fastStructure output file
+    :param fmt: string, format of the indfile_name. Can be either structure of
+    faststructure
+    :param popfile: string. path to file with population list
+    """
 
-    return Qvalues
+    # Parse popfile if provided
+    if popfile:
+        # Assuming popfile has 2 columns with pop name in 1st column and number
+        # of samples in the 2nd column
+        poparray = np.genfromtxt(popfile)
+        # Final pop list
+        poplist = [(x, y) for x, y in zip(np.cumsum([x[1] for x in poparray]),
+                                          [x[0] for x in poparray])]
+
+    else:
+        poplist = []
+
+    # Parse structure/faststructure output file
+    if fmt == "faststructure":
+        qvalues = np.genfromtxt(indfile_name)
+
+    else:
+        qvalues = np.array([])
+
+        # Start file parsing
+        r = False
+        with open(indfile_name) as file_handle:
+            for line in file_handle:
+                if line.strip().lower().startswith("inferred ancestry of "
+                                                   "individuals:"):
+                    # Enter parse mode ON
+                    r = True
+                    # Skip subheader
+                    next(file_handle)
+                elif line.strip().lower().startswith("estimated allele "
+                                                     "frequencies in each "
+                                                     "cluster"):
+                    # parse mode OFF
+                    r = False
+                elif r:
+                    if line.strip() != "":
+                        fields = line.strip().split()
+                        # Get cluster values
+                        cl = [float(x) for x in fields[5:]]
+                        try:
+                            qvalues = np.vstack((qvalues, cl))
+                        except ValueError:
+                            qvalues = np.array(cl)
+                        if not popfile:
+                            # Get population
+                            poplist.append(int(fields[3]))
+
+        if not popfile:
+            # Transform poplist in convenient format,in which each element
+            # is the boundary of a population in the x-axis
+            poplist = Counter(poplist)
+            poplist = [(x, None) for x in np.cumsum(list(poplist.values()))]
+
+    return qvalues, poplist
 
 
-def plotter(Qvalues):
-    """Plot the Qvalues histogram."""
+def plotter(qvalues, poplist):
+    """
+    Plot the qvalues histogram.
+
+    :param qvalues: numpy array with variable shape containing the inferred
+    ancestry values for each sample
+    :param poplist: list, contains information on the sample's population.
+    Must be a list of tuples, in which each element consists of
+    (x-axys position int, population label str). Example:
+    [(2, "Angola"), (5, "Kenya")...]
+    """
 
     colors = ('#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c',
               '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928')
 
-    numinds = Qvalues.shape[0]
-    for i in range(Qvalues.shape[1]):
+    plt.style.use("ggplot")
+
+    numinds = qvalues.shape[0]
+
+    # Update plot width according to the number of samples
+    plt.rcParams["figure.figsize"] = (8 * numinds * .05, 6)
+
+    for i in range(qvalues.shape[1]):
         if i == 0:
-            plt.bar(range(numinds), Qvalues[:, i], facecolor=colors[i],
+            plt.bar(range(numinds), qvalues[:, i], facecolor=colors[i],
                     edgecolor="none", width=1)
-            formerQ = Qvalues[:, i]
+            formerQ = qvalues[:, i]
         else:
-            plt.bar(range(numinds), Qvalues[:, i], bottom=formerQ,
+            plt.bar(range(numinds), qvalues[:, i], bottom=formerQ,
                     facecolor=colors[i], edgecolor="none", width=1)
-            formerQ = formerQ + Qvalues[:, i]
+            formerQ = formerQ + qvalues[:, i]
+
+    # Set lines delimiting populations
+    for i in poplist:
+        plt.axvline(x=i[0], linewidth=1.5, color='black')
 
     plt.ylim(0, 1)
+    plt.xlim(0, numinds)
 
     plt.yticks([])
 
-    # TODO: Add pop labels and split bars
     plt.show()
 
 if __name__ == "__main__":
-    data = dataminer("/home/francisco/Dropbox/Science/PhD/GBS/Analyses/clust6/structure/tests/k3.3.meanQ")
-    plotter(data)
+    data, pops = dataminer("/home/diogo/Diogo/Science/Scripts/packages/Structure_threader/TestData/Test/K3_rep1_f", "structure")
+    plotter(data, pops)
