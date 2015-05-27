711dafcb3863955680a95416c67a508f403c9a05
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-05-27 16:36:47 +0100

    Added a new linear benchmark for fastStructure.

---
+++ benchmarks/benchmark_fast.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+ks=( 1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4 )
+
+for i in $ks
+do
+python2 ~/Software/fastStructure/structure.py -K $i --input=TestBigData --output=TestBigData_out --format=str
+done
