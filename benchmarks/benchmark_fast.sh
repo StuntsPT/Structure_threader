2451d5576861dbaf4bb9910bc4c2ce3040b8e846
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-08-13 00:01:00 +0100

    Improved the benchmarking script.

--- benchmarks/benchmark_fast.sh
+++ benchmarks/benchmark_fast.sh
@@ -5,8 +5,8 @@ ks=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 )
 datafile=Chr1
 #datafile=Chr22
 
-for i in $ks
+for i in ${ks}
 do
-  echo "Currently running K=$i."
-  python2 ~/Software/fastStructure/structure.py -K $i --input=../TestData/Chr1 --output=../TestData/Chr1_out --format=str
+  echo "Currently running K=${i}."
+  python2 ~/Software/fastStructure/structure.py -K $i --input=../TestData/${datafile} --output=../TestData/${datafile}_out --format=str
 done
