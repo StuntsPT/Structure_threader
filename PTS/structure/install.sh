17638050aa3be1d72d738033e299513290dee454
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-08-28 00:49:40 +0100

    Corrected the install script.

--- PTS/structure/install.sh
+++ PTS/structure/install.sh
@@ -8,16 +8,14 @@ echo $? > ~/install-exit-status
 cd ..
 
 echo "#!/bin/bash
-cd structure_kernel_src/
-cp ../mainparams ./
-cp ../extraparams ./
+
 for i in {1..4}
   do
       for j in {1..4}
         do
-  ./structure -i ../Reduced_dataset.structure -o ../output -K ${i} >> \$LOG_FILE 2>&1
+  ./structure_kernel_src/structure -i Reduced_dataset.structure -o output -K \$i >> \$LOG_FILE 2>&1
         done
   done
 
-echo \$? > ~/test-exit-status" > structure.sh
-chmod +x structure.sh
+echo \$? > ~/test-exit-status" > structure
+chmod +x structure
