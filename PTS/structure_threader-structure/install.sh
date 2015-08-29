0c2023ebda8013d2648d9a541aebfedaec6cfb3e
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-08-29 01:29:49 +0100

    Added a forgotten file.

---
+++ PTS/structure_threader-structure/install.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+# Define test dir
+testdir=`pwd`
+
+# Get LDFAGS
+_OLD_LDFLAGS=$LDFLAGS
+
+# Create env dir for dependent libraries
+mkdir -p ${testdir}/env
+envdir=${testdir}/env
+
+# Install dependencies
+# LAPACK
+tar xvfz lapack-3.5.0.tgz
+mkdir -p build-lapack
+cd build-lapack
+cmake ../lapack-3.5.0
+make
+mkdir -p ${envdir}/{bin,lib}
+mv bin/* ${envdir}/bin
+mv lib/* ${envdir}/lib
+
+# cython
+cd ${testdir}
+unzip Cython-0.22.zip
+cd Cython-0.22
+python2 setup.py install --target=${envdir}
+
+# python-nose
+cd ${testdir}
+tar xvfz nose-1.3.6.tar.gz
+cd nose-1.3.6
+python2 setup.py install --target=${envdir}
+
+# numpy
+cd ${testdir}
+tar xvfz numpy.tar.gz
+cd numpy-1.9.2
+sed -e "s|#![ ]*/usr/bin/python$|#!/usr/bin/python2|" \
+    -e "s|#![ ]*/usr/bin/env python$|#!/usr/bin/env python2|" \
+    -e "s|#![ ]*/bin/env python$|#!/usr/bin/env python2|" \
+    -i $(find . -name '*.py')
+export ATLAS=None
+export LDFLAGS="$LDFLAGS -shared"
+python2 setup.py install --target=${envdir}
+
+# scipy
+cd ${testdir}
+tar xvfz scipy.tar.gz
+cd scipy-0.16.0b2
+python2 setup.py install --target=${envdir}
+
+# GNU scientific library
+cd ${testdir}
+tar xvzf gsl-latest.tar.gz
+cd gsl-1.16
+export LDFLAGS=${_OLD_LDFLAGS}
+./configure --prefix=${envdir}
+make
+make install
+
+# fastStructure
+# Extract tarball, enter src dir, build binary and place it in the env dir
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${envdir}/lib
+export CFLAGS="-I${envdir}/include"
+export LDFLAGS="-L${envdir}/lib"
+cd ${testdir}
+tar xvfz fastStructure-1.0.tar.gz
+cd  fastStructure-1.0
+cd vars
+python2 setup.py build_ext --inplace
+cd ..
+python2 setup.py build_ext --inplace
+
+# Structure
+cd ${testdir}
+tar -zxvf structure_kernel_source.tar.gz
+cd structure_kernel_src
+make
+
+# Structure_threader
+cd ${testdir}
+tar xfvz v0.1-rc3.tar.gz
+
+# Create launcher script
+
+echo "#!/bin/bash
+cd Structure_threader-0.1-rc3/
+python3 structure_threader.py -i ../Reduced_dataset.structure -o ../ -K 4 -R 4 -t \$NUM_CPU_CORES -st ../structure_kernel_src/structure --no-plots 1 --no-tests 1 > \$LOG_FILE 2>&1
+" > structure_threader
+chmod +x structure_threader
