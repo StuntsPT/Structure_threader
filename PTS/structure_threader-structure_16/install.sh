d3288321747fa3440c7055bb4de09ec50ab865fc
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-08-31 03:21:09 +0100

    Added a 16 threads test version.

---
+++ PTS/structure_threader-structure_16/install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Define test dir
+testdir=`pwd`
+
+# Create env dir for dependent libraries
+mkdir -p ${testdir}/env
+
+# Install dependencies
+# Structure
+cd ${testdir}
+tar -zxvf structure_kernel_source.tar.gz
+cd structure_kernel_src
+make
+
+# Install Structure_threder
+# Structure_threader
+cd ${testdir}
+tar xfvz v0.1-rc3.tar.gz
+
+# Create launcher script
+
+echo "#!/bin/bash
+workdir=`pwd`
+cd Structure_threader-0.1-rc3/
+python3 structure_threader.py \$@ -st \${workdir}/structure_kernel_src/structure > \$LOG_FILE 2>&1
+" > structure_threader-structure_16
+chmod +x structure_threader-structure_16
