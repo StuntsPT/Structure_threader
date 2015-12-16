3e32bc1fd5e81fe97f7a06528fc76e9caa6656ac
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-12-16 14:57:40 +0000

    Removed deprecated usage doc.

--- plotter/structplot.py
+++ plotter/structplot.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with structure_threader. If not, see <http://www.gnu.org/licenses/>.
 
-# Usage: python3 structplot.py infile outfile
 
 import matplotlib
 matplotlib.use('Agg')
