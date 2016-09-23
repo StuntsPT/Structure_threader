d1eafbdb9175d630124fb794e90c1d1c9e1ea106
Francisco Pina Martins <f.pinamartins@gmail.com>
2016-09-24 00:31:09 +0100

    Updated copyright.

--- structure_threader/evanno/fastChooseK.py
+++ structure_threader/evanno/fastChooseK.py
@@ -22,7 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# Copyright 2015 Francisco Pina Martins <f.pinamartins@gmail.com>
+# Copyright 2015-2016 Francisco Pina Martins <f.pinamartins@gmail.com>
 # This file is part of structure_threader.
 # structure_threader is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
