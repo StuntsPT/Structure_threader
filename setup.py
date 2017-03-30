be6caebbc7ca3973f09e96e05298de11592fb4ed
Francisco Pina Martins <f.pinamartins@gmail.com>
2017-03-30 11:11:34 +0100

    Bumped package version.

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ def platform_detection(install_binaries=True):
 
 # Set some variables (PKGBUILD inspired)
 DATA_FILES = platform_detection()
-VERSION = "0.4.1"
+VERSION = "0.4.2"
 URL = "https://github.com/StuntsPT/Structure_threader"
 
 
