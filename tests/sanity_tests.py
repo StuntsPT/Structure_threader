d2dc126f77455253bbf541d573b46d0ac09316a7
Francisco Pina Martins <f.pinamartins@gmail.com>
2017-06-15 16:18:27 +0100

    Better PEP8 conformance.

--- tests/sanity_tests.py
+++ tests/sanity_tests.py
@@ -15,10 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with structure_threader. If not, see <http://www.gnu.org/licenses/>.
 
-import pytest
 import os
+import pytest
 import structure_threader.sanity_checks.sanity as sc
-
 
 
 def test_cpu_checker():
