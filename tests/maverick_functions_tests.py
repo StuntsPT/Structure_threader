5fa08baee6e3583f85c9139ff43e36d61b18f0fe
Francisco Pina Martins <f.pinamartins@gmail.com>
2017-06-20 11:25:43 +0100

    Improved testing.

--- tests/maverick_functions_tests.py
+++ tests/maverick_functions_tests.py
@@ -18,8 +18,67 @@
 import hashlib
 import os
 import pytest
-import structure_threader.structure_threader as st
 import structure_threader.wrappers.maverick_wrapper as mw
+
+class Arguments():
+    """
+    Bogus class to work a mock for the "args" attributes from argparse.
+    """
+    def __init__(self):
+        self.external_prog = "EP"
+        self.infile = "IF"
+        self.outpath = ""
+        self.params = "smalldata/parameters.txt"
+        self.notests = False
+        self.k_list = [2, 3, 4, 5]
+
+
+def test_mav_cli_generator():
+    """
+    Tests if mav_cli_generator() is working correctlly.
+    """
+    # Define arguments
+    arg = Arguments()
+    k_val = 4
+
+    mock_cli = ["EP", "-Kmin", str(k_val), "-Kmax", str(k_val), "-data",
+                "IF", "-outputRoot", "mav_K4/", "-masterRoot", "/",
+                "-parameters", "smalldata/parameters.txt"]
+
+    # Perform test with and without TI
+    for ti_value in (False, True):
+        arg.notests = ti_value
+        if arg.notests is True:
+            mock_cli += ["-thermodynamic_on", "f"]
+
+        returned_cli, out_dir = mw.mav_cli_generator(arg, k_val)
+        assert returned_cli == mock_cli
+        assert out_dir == "mav_K4/"
+
+
+def test_mav_params_parser():
+    """
+    Tests if mav_params_parser() is working correctlly.
+    """
+    assert mw.mav_params_parser("smalldata/parameters.txt") is True
+    assert mw.mav_params_parser("smalldata/parameters_f.txt") is False
+
+
+def test_mav_alpha_failsafe():
+    """
+    Tests if mav_alpha_failsafe() is working correctlly.
+    """
+    k_list = [2, 3, 4, 5]
+    assert mw.mav_alpha_failsafe("smalldata/parameters.txt", k_list) == {
+        "alpha": False, "alphaPropSD": False}
+    assert mw.mav_alpha_failsafe("smalldata/parameters_a.txt", k_list) == {
+        "alpha": {2: "0.9", 3: "0.8", 4: "0.7", 5: "0.6"}, "alphaPropSD": False}
+    assert mw.mav_alpha_failsafe("smalldata/parameters_as.txt", k_list) == {
+        "alpha": False,
+        "alphaPropSD": {2: "0.09", 3: "0.08", 4: "0.07", 5: "0.06"}}
+    assert mw.mav_alpha_failsafe("smalldata/parameters_a_as.txt", k_list) == {
+        "alpha": {2: "0.9", 3: "0.8", 4: "0.7", 5: "0.6"},
+        "alphaPropSD": {2: "0.09", 3: "0.08", 4: "0.07", 5: "0.06"}}
 
 
 def test_maverick_merger():
