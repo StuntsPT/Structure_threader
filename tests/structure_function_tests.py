f518f46199eebda2020011c593ec26c278a49190
Francisco Pina Martins <f.pinamartins@gmail.com>
2017-06-20 14:54:58 +0100

    Added a test for STRUCTURE.

---
+++ tests/structure_function_tests.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+# Copyright 2017 Francisco Pina Martins <f.pinamartins@gmail.com>
+# This file is part of structure_threader.
+# structure_threader is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# structure_threader is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with structure_threader. If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+import mockups
+import structure_threader.wrappers.structure_wrapper as sw
+
+
+def test_str_cli_generator():
+    """
+    Tests if str_cli_generator() is working correctlly.
+    """
+    # Define arguments
+    arg = mockups.Arguments()
+    k_val = 4
+    outfile = "str_K4_rep1"
+    arg.params = None
+
+    mock_cli = ["EP", "-K", str(k_val), "-i", "IF", "-o", outfile]
+    returned_cli, returned_outfile = sw.str_cli_generator(arg, k_val, 1)
+
+    assert returned_cli == mock_cli
+    assert returned_outfile == outfile
+
+    arg.params = "test"
+
+    mock_cli += arg.params
+    returned_cli, returned_outfile = sw.str_cli_generator(arg, k_val, 1)
+
+    assert returned_cli == mock_cli
+    assert returned_outfile == outfile
