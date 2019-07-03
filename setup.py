8aa6de4734f1691083766eda7f6eddb59154a3be
Francisco Pina Martins <f.pinamartins@gmail.com>
2019-07-03 16:19:12 +0100

    Updates copyright

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright 2016-2018 Francisco Pina Martins <f.pinamartins@gmail.com>
+# Copyright 2016-2019 Francisco Pina Martins <f.pinamartins@gmail.com>
 # This file is part of structure_threader.
 # structure_threader is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
