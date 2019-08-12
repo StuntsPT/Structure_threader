0c9bc68e8f430ca2f289f24d927cbbc26bb152d9
Francisco Pina Martins <f.pinamartins@gmail.com>
2019-08-12 17:39:39 +0100

    Adds a field test for ALStructure with a VCF file and test data

--- tests/alstructure_field_tests.sh
+++ tests/alstructure_field_tests.sh
@@ -29,4 +29,9 @@ structure_threader_exec=$(which structure_threader)
 tar xvfJ ${git_dir}/tests/smalldata/BigTestData.bed.tar.xz -C ${git_dir}/tests/smalldata/
 ${structure_threader_exec} run -i ${git_dir}/tests/smalldata/BigTestData.bed -o ~/results_als -als ${str_bin} -K 4 -t 4 --ind ${git_dir}/tests/smalldata/indfile.txt
 
-echo -e "${LightGreen}ALStructure 'Field test' ran successfully. Yay!${NoColor}"
+echo -e "${LightGreen}ALStructure 'Field test' ran successfully on the \`.bed\` file. Yay!${NoColor}"
+
+tar xvfJ ${git_dir}/tests/smalldata/BigTestData.vcf.tar.xz -C ${git_dir}/tests/smalldata/
+${structure_threader_exec} run -i ${git_dir}/tests/smalldata/BigTestData.vcf -o ~/results_als -als ${str_bin} -K 4 -t 4 --ind ${git_dir}/tests/smalldata/indfile.txt
+
+echo -e "${LightGreen}ALStructure 'Field test' ran successfully on the \`.vcf\` file. Yay!${NoColor}"
